c501d1318ea0b5f33cf2bf2e0a150a1aa0dded89
http://jneen.net/ <jneen@jneen.net>
2021-09-05 14:23:22 -0400

    only ignore the root config.js

---
+++ ui/reducers/config.js
@@ -0,0 +1,19 @@
+import { INIT_STATE } from '../constants/ActionTypes';
+
+const initialState = {};
+
+export default function reduce(state = initialState, action = {}) {
+  const { type, payload } = action;
+  switch (type) {
+    case INIT_STATE:
+      if (payload.roles) {
+        return {
+          ...state,
+          roles: payload.roles,
+        };
+      }
+      return state;
+    default:
+      return state;
+  }
+}
